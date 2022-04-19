2a1fb3f8209997ea68802b3287c87c31ce9019c4
oriana143 <95234045+oriana143@users.noreply.github.com>
2022-04-19 02:40:20 +0200

    commit

--- Assets/Scrips/EnemyMover.cs
+++ Assets/Scrips/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<WayPoint> path = new List<WayPoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range (0f, 5f)]float speed = 1f;
     
     void Start()
     {
@@ -16,8 +16,21 @@ public class EnemyMover : MonoBehaviour
     {
         foreach (WayPoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime); 
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp (startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+
+
+            
         }
     }
 }
